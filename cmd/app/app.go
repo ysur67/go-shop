f7ed0f746ffb33e371893110ff6216f9989c9533
ysur67 <rus.muratow@gmail.com>
2021-11-03 10:58:08 +0300

    fix: remove print

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	server "shop/server"
 )
 
 func main() {
 	srv := server.NewApp()
 	if err := srv.Run("4444"); err != nil {
-		fmt.Println("asdfsf")
 		panic(err)
 	}
 }
